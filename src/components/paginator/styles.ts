fea743dfb8e5d762cd6503a7474938c867ad7f38
junior-apostolo <juniorapostolo2010@gmail.com>
2024-08-21 22:28:03 -0300

    feat: cria componente de paginator

---
+++ src/components/paginator/styles.ts
@@ -0,0 +1,11 @@
+import { theme } from "@/theme";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    dot: {
+        height: 10,
+        borderRadius: 5,
+        backgroundColor: theme.colors.bg_color,
+        marginHorizontal: 8,
+    }
+})
