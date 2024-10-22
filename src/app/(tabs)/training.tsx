dcd253cb45b3cea8c5fee2e921b607a5c9287669
Vinicius Leal <viniciusleal@MacBook-Pro-de-Vinicius.local>
2024-10-21 23:36:18 -0300

    metodo biset

--- src/app/(tabs)/training.tsx
+++ src/app/(tabs)/training.tsx
@@ -8,47 +8,46 @@ import { LayoutAnimation, ScrollView, StyleSheet, Text, TouchableOpacity, View }
 import { getAllExerciseTraining } from '@/services/exerciseTraining';
 
 const Training: React.FC = () => {
-    const [expandedSection, setExpandedSection] = useState<string | null>(null); // Control which section is expanded
+    const [expandedSection, setExpandedSection] = useState<string | null>(null);
     const [exercises, setExercises] = useState<Array<TrainingDetail>>([]);
     const { id, image, nome, description } = useLocalSearchParams();
+    
     const toggleExpand = (section: string) => {
         LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
         setExpandedSection(section === expandedSection ? null : section);
     }
 
     const nextStep = (item: TrainingDetail) => {
-
         router.push({
             pathname: "(tabs)/exercise",
             params: {
-             nomeExercicio: item.exercise.nome,
-             linkVideo: item.exercise.linkVideo,
-             descriptionExercicio: item.exercise.descricao,
-             observacao: item.observacao,
-             id: id,
-             nome: nome,
-             image: image,
-             description: description
+                nomeExercicio: item.exercise.nome,
+                linkVideo: item.exercise.linkVideo,
+                descriptionExercicio: item.exercise.descricao,
+                observacao: item.observacao,
+                id: id,
+                nome: nome,
+                image: image,
+                description: description
             },
-          })
+        })
     };
    
     const loadingExercises = async() => {
-        try{
+        try {
             const response = await getAllExerciseTraining(id);
-            if(!response){
+            if (!response) {
                 return false;
             }
-            setExercises(response)
-        }catch(err){
+            setExercises(response);
+        } catch (err) {
             return false;
         }
     }
 
-    useEffect(()=> {
-        loadingExercises()
-    },[id])
-
+    useEffect(() => {
+        loadingExercises();
+    }, [id]);
 
     return (
         <View style={styles.container}>
@@ -67,14 +66,29 @@ const Training: React.FC = () => {
                 />
 
                 <View style={styles.exerciseList}>
-                    {exercises.map((exercise, index) => (
-                        <TouchableOpacity key={index} style={styles.exerciseButton} onPress={() => nextStep(exercise)}>
-                            <Text style={styles.exerciseText}>
-                                {exercise.exercise.nome}
-                            </Text>
-                            <Text style={styles.repText}>{exercise?.serie} - {exercise?.rep}</Text>
-                        </TouchableOpacity>
-                    ))}
+                    {exercises.map((exercise, index) => {
+                        const isBiset = exercise.metodo.toLocaleLowerCase() === 'biset';
+                        const isLastBiset = isBiset && (index === exercises.length - 1 || exercises[index + 1]?.metodo !== 'biset');
+                        return (
+                            <TouchableOpacity 
+                                key={index} 
+                                style={[
+                                    styles.exerciseButton,
+                                    isLastBiset && styles.lastBiset,
+                                    exercise.metodo.toLocaleLowerCase() != "biset" && {marginTop: 10},
+                                    exercises[index + 1]?.metodo.toLocaleLowerCase() !== 'biset' && {borderBottomWidth: 4}
+                                ]}
+                                onPress={() => nextStep(exercise)}
+                            >
+                                <Text style={styles.exerciseText}>
+                                    {exercise.exercise.nome}
+                                </Text>
+                                <Text style={styles.repText}>
+                                    {exercise?.serie} - {exercise?.rep}
+                                </Text>
+                            </TouchableOpacity>
+                        );
+                    })}
                 </View>
             </ScrollView>
         </View>
@@ -90,10 +104,6 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         paddingTop: 70
     },
-    content: {
-        width: "100%",
-        alignItems: "center"
-    },
     exerciseList: {
         marginTop: 20,
         width: '100%',
@@ -105,11 +115,16 @@ const styles = StyleSheet.create({
         backgroundColor: colors.blue_750,
         paddingVertical: 15,
         paddingHorizontal: 20,
-        borderBottomWidth: 4,  // Borda inferior de 4px
+        borderBottomWidth: 4,
         borderBottomColor: colors.green_100,
         borderRadius: 10,
         marginBottom: 10,
         minHeight: 50
+    },
+    lastBiset: {
+        borderBottomWidth: 0, 
+        marginBottom: 0,
+        borderRadius: 0,
     },
     exerciseText: {
         fontSize: 16,
